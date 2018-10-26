602361b8e3cb2db8cbd41f4f62b46a93def7c149
ylee02 <ylee01@stuy.edu>
2018-10-26 14:31:49 +0900

    Updated File

--- SuperArray.java
+++ SuperArray.java
@@ -16,7 +16,7 @@ public class SuperArray {
   }
 
   public boolean add(String element) {
-    for (int i = 0; i < data.length; i++) {
+    for (int i = 0; i < size; i++) {
       if (data[i].equals("")) {
         data[i] = element;
         return true;
@@ -32,16 +32,64 @@ public class SuperArray {
     temp += "]";
     return temp;
   }
+  
+  public boolean contains(String element) {
+	for (int i = 0; i < size; i++) {
+		if (data[i].equals(element) {
+			return true;
+		}
+	}
+	return false;
+  }
 
   public boolean add(int index, String element) {
     String[] newArray = new String[size + 1];
+	size += 1;
     for (int i = 0; i < size + 1; i++) {
-        newArray[i] = data[i];
-        if (i == index) {
+		if (i == index) {
           i += 1;
         }
+        newArray[i] = data[i];
+        
     }
     newArray[index] = element;
+	data = newArray;
     return true;
+  }
+  
+  public int indexOf(String element) {
+	for (int i = 0; i < size; i++) {
+		if (data[i].equals(element) {
+			return i;
+		}
+	}
+	return -1;
+  }
+  
+  public int lastIndexOf(String element) {
+	 int counter = -1;
+	 for (int i = 0; i < size; i++) {
+		if (data[i].equals(element) {
+			counter = i;
+		}
+	 }
+	 return counter;
+  }
+  
+  public String remove(int index) {
+	  String[] newArray = new String[size - 1];
+	  size -= 1;
+	  int counter = 0;
+	  String counter2 = "";
+	  for (int i = 0; i < size - 1; i++){ 
+		if (i == index){ 
+			counter2 = data[i]
+			i += 1;
+		}
+		newArray[counter] = data[i];
+		counter += 1;
+	  }
+	  data = newArray;
+	  return counter2;
   }
 }
